3a2d38d39f8f1ac3e3466b744112351cd634056e
mbwika <collinsmbwika10@gmail.com>
2025-05-06 21:52:38 -0500

    Update environment variable names and modify API endpoints in Login and Register components

--- frontend/src/components/Login/Register.jsx
+++ frontend/src/components/Login/Register.jsx
@@ -40,6 +40,7 @@ const Register = () => {
 
     try {
       const response = await fetch("http://localhost:5000/api/register", {
+        // const response = await fetch("https://tafsiri.creativedisturbance.org:5000/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
